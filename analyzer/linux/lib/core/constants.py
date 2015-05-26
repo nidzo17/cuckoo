f3e018895156044065114cb135b2f86dde661c01
Nikola <nikola.ravnjak@gmail.com>
2015-05-26 03:27:25 +0200

    * some more work

---
+++ analyzer/linux/lib/core/constants.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2010-2015 Cuckoo Foundation.
+# This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
+# See the file 'docs/LICENSE' for copying permission.
+
+import os
+
+
+ROOT = '/home/nidzo'
+
+PATHS = {"root": ROOT,
+         "temp": os.path.join(ROOT, "tmp"),
+         "logs": os.path.join(ROOT, "logs"),
+         "files": os.path.join(ROOT, "files"),
+         "shots": os.path.join(ROOT, "shots"),
+         "memory": os.path.join(ROOT, "memory"),
+         "drop": os.path.join(ROOT, "drop")}
